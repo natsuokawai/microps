d0d14abff76ea40d3245304c041683a6dea21e27
natsuokawai <kawai.natsuo@gmail.com>
2023-08-18 09:46:38 +0900

    feat: implement ip_addr_pton() and ip_addr_ntop()

--- ip.h
+++ ip.h
@@ -1,7 +1,17 @@
 #ifndef IP_H
 #define IP_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+typedef uint32_t ip_addr_t;
+
 extern int
 ip_init(void);
+
+extern int
+ip_addr_pton(const char *p, ip_addr_t *n);
+extern char *
+ip_addr_ntop(ip_addr_t *n, const char *p, size_t size);
 
 #endif
